a87bba6a29a78b2a287f7acc97f50e138aa968e6
firetending <alan.miller.375@gmail.com>
2022-06-07 00:47:29 -0400

    cleanup

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ window.addEventListener("load", function () {
     let flightStatus = this.document.getElementById("flightStatus");
     let shuttleBackground = this.document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
-    let spaceShuttleHeightValue = Number(spaceShuttleHeight.innerHTML);
     let landing = this.document.getElementById("landing");
     let missionAbort = this.document.getElementById("missionAbort");
     let up = this.document.getElementById("up");
@@ -16,73 +15,105 @@ window.addEventListener("load", function () {
     let rocket = this.document.getElementById("rocket");
     let statusInFlight = "Shuttle in flight.";
 
-    takeoff.addEventListener("click", function (event) {
-        let readyForTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff.");
-        if (readyForTakeoff) {
-            flightStatus.innerHTML = statusInFlight;
-            shuttleBackground.style.background = "blue";
-            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
-        }
-
-    });
-
-    landing.addEventListener("click", function () {
-        window.alert("The shuttle is landing. Landing gear engaged.");
-        flightStatus.innerHTML = "The shuttle has landed.";
-        shuttleBackground.style.background = "green";
-        spaceShuttleHeight.innerHTML = 0;
-    });
-
-    missionAbort.addEventListener("click", function () {
-        let confirmAbort = window.confirm("Confirm that you want to abort the mission.");
-        if (confirmAbort) {
-            flightStatus.innerHTML = "Mission aborted";
-            shuttleBackground.style.background = "green";
-            spaceShuttleHeight.innerHTML = 0;
-        }
-    });
-
+    //set rocket position within shuttleBackground
     rocket.style.position = "absolute";
-    //rocket.style.top = "0px";
     rocket.style.bottom = "0px";
     rocket.style.left = "0px";
-    rocket.style.right = "0px";
+
     function printRocketPosition() {
         console.log("top: " + rocket.style.top);
         console.log("bottom: " + rocket.style.bottom);
         console.log("left: " + rocket.style.left);
         console.log("right: " + rocket.style.right);
     }
-    printRocketPosition();
 
-    up.addEventListener("click", function (event) {
-        if (flightStatus.innerHTML !== statusInFlight) {
-            window.alert("Rocket must be in flight first!");
+    function isInFlight(alertWhenBoolean) {
+        let output = (flightStatus.innerHTML === statusInFlight)
+        if (output) {
+            if (alertWhenBoolean === "alertWhenTrue") {
+                window.alert("Shuttle is already flight!");
+            }
+        } else {
+            if (alertWhenBoolean === "alertWhenFalse") {
+                window.alert("Shuttle is not in flight!");
+            }
+        }
+        return output;
+    }
+
+    function moveRocket(direction, px = 10) {
+        if (!isInFlight("alertWhenFalse")) {
             return;
         }
-        rocket.style.bottom = parseInt(rocket.style.bottom) + 10 + "px";
-        spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+        if (direction === "up") {
+            rocket.style.bottom = parseInt(rocket.style.bottom) + px + "px";
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) + 10000;
+        } else if (direction === "down") {
+            rocket.style.bottom = parseInt(rocket.style.bottom) - px + "px";
+            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+        } else if (direction === "left") {
+            rocket.style.left = parseInt(rocket.style.left) - px + "px";
+        } else if (direction === "right") {
+            rocket.style.left = parseInt(rocket.style.left) + px + "px";
+        } else if (direction === "land" || direction === "abort") {
+            rocket.style.bottom = "0px";
+            shuttleBackground.style.background = "green";
+            spaceShuttleHeight.innerHTML = 0;
+            if (direction === "land") {
+                window.alert("The shuttle is landing. Landing gear engaged.");
+                flightStatus.innerHTML = "The shuttle has landed.";
+            }
+        }
+
         printRocketPosition();
+    }
+
+    //event listeners
+    takeoff.addEventListener("click", function (event) {
+        if (isInFlight("alertWhenTrue")) {
+            return;
+        }
+        let readyForTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (readyForTakeoff) {
+            flightStatus.innerHTML = statusInFlight;
+            shuttleBackground.style.background = "blue";
+            moveRocket("up");
+        }
+    });
+
+    landing.addEventListener("click", function () {
+        moveRocket("land");
+
+    });
+
+    missionAbort.addEventListener("click", function () {
+        let confirmAbort = window.confirm("Confirm that you want to abort the mission.");
+        if (confirmAbort) {
+            if (isInFlight()) {
+                moveRocket("abort");
+            }
+            flightStatus.innerHTML = "Mission aborted";
+        }
+    });
+
+    up.addEventListener("click", function (event) {
+        moveRocket("up");
     });
 
     down.addEventListener("click", function (event) {
         if (Number(spaceShuttleHeight.innerHTML) >= 20000) {
-            rocket.style.bottom = parseInt(rocket.style.bottom) - 10 + "px";
-            spaceShuttleHeight.innerHTML = Number(spaceShuttleHeight.innerHTML) - 10000;
+            moveRocket("down");
         } else {
             window.alert("Cannot descend below 10,000 miles without landing or aborting mission!");
         }
-        printRocketPosition();
     });
 
     left.addEventListener("click", function (event) {
-        rocket.style.left = parseInt(rocket.style.left) - 10 + "px";
-        printRocketPosition();
+        moveRocket("left");
     });
 
     right.addEventListener("click", function (event) {
-        rocket.style.left = parseInt(rocket.style.left) + 10 + "px";
-        printRocketPosition();
+        moveRocket("right");
     });
 
 });
